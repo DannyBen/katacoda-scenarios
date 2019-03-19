5459ba0a86259628e9e10afe7771739008dc977c
Danny Ben Shitrit <db@dannyben.com>
2019-03-19 18:02:49 +0000

    Updated

--- jobly/bootstrap.sh
+++ jobly/bootstrap.sh
@@ -3,10 +3,5 @@ apt-add-repository -y ppa:brightbox/ruby-ng
 apt-get -y update
 apt-get -y install ruby2.5 ruby2.5-dev
 
-cat >~/.gemrc <<EOL
-install: --no-rdoc --no-ri
-update:  --no-rdoc --no-ri
-EOL
-
 gem install jobly
 jobly --version
