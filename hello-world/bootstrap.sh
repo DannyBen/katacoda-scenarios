c4e1577553f5ab66c8665668d6b6b1eac266990a
Danny Ben Shitrit <db@dannyben.com>
2019-03-20 21:24:12 +0000

    Updated

--- hello-world/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-echo "--- Installing Ruby"
-apt-add-repository -y ppa:brightbox/ruby-ng
-apt-get -y update
-apt-get -y install ruby2.5 ruby2.5-dev
-
-echo "--- Done Ruby Installation"
-ruby --version
