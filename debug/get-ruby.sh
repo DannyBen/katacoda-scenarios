ddf8dd89dcb09d977864236dcb3604f366616227
Danny Ben Shitrit <db@dannyben.com>
2019-03-20 06:12:17 +0000

    Updated

--- debug/get-ruby.sh
+++ debug/get-ruby.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 apt-add-repository -y ppa:brightbox/ruby-ng
 apt-get -y update
-apt-get -y install ruby2.5 ruby2.5-dev
-
-echo "Your Ruby is Ready"
+apt-get -y install ruby2.5 ruby2.5-dev < /dev/null
 ruby --version
