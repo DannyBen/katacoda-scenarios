29c2aa19090faaa9c6d6ae849dcc3e78651036ba
Danny Ben Shitrit <db@dannyben.com>
2019-03-20 06:19:34 +0000

    Updated

--- jobly/bootstrap.sh
+++ jobly/bootstrap.sh
@@ -1,3 +1,6 @@
 apt-add-repository -y ppa:brightbox/ruby-ng
 apt-get -y update
-apt-get -y install ruby2.5 ruby2.5-dev redis-server && gem install jobly
+apt-get -y install ruby2.5 ruby2.5-dev redis-server < /dev/null
+gem install jobly
+ruby --version
+jobly --version
